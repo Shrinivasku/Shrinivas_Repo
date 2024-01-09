e314f767f36388843459e84202de0a462c075a43
s.arun.kulkarni <s.arun.kulkarni@bdc4-l-CPWWD63.dir.svc.accenture.com>
2024-01-09 11:50:21 +0530

    test suite commit

---
+++ Test Suites/Login with invalid cred/Login with invalid cred.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login with invalid cred</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>75dba381-6eab-4d67-bf9a-eae58d67c1ef</testSuiteGuid>
+   <testCaseLink>
+      <guid>84f5b184-236b-47c6-b74c-8f1f9ef242be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeHRM_invalid_login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
